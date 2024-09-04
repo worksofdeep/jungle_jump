952c66a0dcea7ecd393ce7d5dd67b1b6ff8273ae
Deepak P <worksofdeep@gmail.com>
2024-09-04 12:03:16 +0530

    Initial state machine code added

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var gravity = 750
+@export var run_speed = 150
+@export var jump_speed = -300
+
+# states
+enum { IDLE, RUN, JUMP, HURT, DEAD }
+var state = IDLE
+
+func _ready():
+	change_state(IDLE)
+	
+func change_state(new_state):
+	state = new_state
+	
+	# animate based on state
+	match state:
+		IDLE: $AnimationPlayer.play("idle")
+		RUN: $AnimationPlayer.play("run")
+		HURT: $AnimationPlayer.play("hurt")
+		JUMP: $AnimationPlayer.play("jump_up")
+		DEAD: hide()
